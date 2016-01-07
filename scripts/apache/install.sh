51468708f1a064c13045fd94b40946459635c22b
rsdf3249 <valentin.boucher@orange.fr>
2016-01-07 11:16:36 +0100

    upadate archive files

---
+++ scripts/apache/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+ctx logger debug "${COMMAND}"
+
+
+sudo apt-get update
+
+sudo apt-get install apache2
+
---
+++ scripts/test.py
@@ -0,0 +1,33 @@
+import json
+from cloudify import ctx
+
+def add_backend(data_path='/var/www/data.json'):
+    with open(data_path) as data_file:
+        data = json.load(data_file)
+
+    name = ctx.source.instance.id
+    data['client'].append({"nom": name})
+
+    with open(data_path, 'w') as outfile:
+        json.dump(data, outfile)
+
+def remove_backend(data_path='/var/www/data.json'):
+    with open(data_path) as data_file:
+        data = json.load(data_file)
+
+    name = ctx.source.instance.id
+    data['client'].remove({"nom": name})
+
+    with open(data_path, 'w') as outfile:
+        json.dump(data, outfile)
+
+def _main():
+    invocation = inputs['invocation']
+    function = invocation['function']
+    args = invocation.get('args', [])
+    kwargs = invocation.get('kwargs', {})
+    globals()[function](*args, **kwargs)
+
+
+if __name__ == '__main__':
+    _main()
