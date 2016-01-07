87cd2fc9be4a9271e1f3fdb520c73e49b6397dd4
rsdf3249 <valentin.boucher@orange.fr>
2016-01-07 14:58:22 +0100

    launch test

--- scripts/nginx/install.sh
+++ scripts/nginx/install.sh
@@ -5,8 +5,8 @@ ctx logger debug "${COMMAND}"
 
 sudo mkdir /var/www
 sudo cp /home/vagrant/cloudify/blueprints/cloudify-test-upgrade/resources/html/* /var/www/
-chown -R nginx:nginx /var/www
-chmod -R 777 /var/www
+sudo chown -R nginx:nginx /var/www
+sudo chmod -R 777 /var/www
 
 sudo cp /home/vagrant/cloudify/blueprints/cloudify-test-upgrade/resources/test.conf /etc/nginx/conf.d/test.conf
 sudo service nginx restart
