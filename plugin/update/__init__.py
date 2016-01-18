d1ed945cac94d85c51d2e76c6318441f991f2028
rsdf3249 <valentin.boucher@orange.fr>
2016-01-18 10:17:26 +0100

    test

--- plugin/update/__init__.py
+++ plugin/update/__init__.py
@@ -10,6 +10,7 @@ def run_operation(operation, nodes_type_update, operation_kwargs, **kwargs):
     send_event_starting_tasks = {}
     send_event_done_tasks = {}
     run_it = False
+    tasks = {}
 
     for node_type_update in nodes_type_update:
         for node in ctx.nodes:
@@ -20,11 +21,13 @@ def run_operation(operation, nodes_type_update, operation_kwargs, **kwargs):
 
 
     for node_type_update in nodes_type_update:
+        tasks[node_type_update] = []
         for node in ctx.nodes:
             if node.type == node_type_update:
                 for instance in node.instances:
 
-                    sequence = graph.sequence()
+                    subgraph = graph.subgraph('some_start_subgraph')
+                    subgraph_sequence = subgraph.sequence()
 
                     #operation_task = instance.execute_operation(operation, kwargs=operation_kwargs)
 
@@ -44,12 +47,20 @@ def run_operation(operation, nodes_type_update, operation_kwargs, **kwargs):
                             forkjoin_tasks_link.append(relationship.execute_target_operation(operation_link))
                     operation_task_link = forkjoin(*forkjoin_tasks_link)
 
-                    sequence.add(
+                    subgraph_sequence.add([
                         send_event_starting_tasks[instance.id],
                         operation_task_unlink,
-                        instance.send_event('Update task !!!'),
+                        instance.send_event('Update task !!'),
                         operation_task_link,
-                        send_event_done_tasks[instance.id])
+                        send_event_done_tasks[instance.id]])
 
+                    tasks[node_type_update].append(subgraph)
+
+    previous_task = None
+    for node_task in tasks:
+        for task in tasks[node_task]:
+            if previous_task:
+                graph.add_dependency(task, previous_task)
+            previous_task = task
 
     return graph.execute()
